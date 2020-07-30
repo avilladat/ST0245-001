3193b0798c59538e683cf2102a6f3bb358411c5c
avilladat <68928365+avilladat@users.noreply.github.com>
2020-07-30 14:59:05 -0500

    Add files via upload

---
+++ talleres/taller01/Linea2D.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class Linea2D {
+    private Punto pInicial;
+    private Punto pFinal;
+
+    public Linea2D(Punto pInicial, Punto pFinal) {
+        this.pInicial = pInicial;
+        this.pFinal = pFinal;
+    }
+    
+    public Punto pi(){
+        return this.pInicial;
+    }
+
+    public Punto pf(){
+        return this.pFinal;
+    }
+
+    public double pendiente(){
+        return (this.pInicial.y()-this.pFinal.y())/(this.pInicial.x()-this.pFinal.x());
+    }
+
+    public ArrayList<Punto> puntos(){
+        ArrayList<Punto> ps = new ArrayList<Punto>();
+        if(this.pInicial.x()<this.pFinal.x()){
+        double x = this.pInicial.x();
+        double y = this.pInicial.y();
+        int v = (int)y;
+        while(x<=this.pFinal.x()){
+            if(v==y)
+                ps.add(new Punto(x,y));
+            x++;
+            y = y + pendiente();
+            v = (int)y;
+        }
+        }else{
+        double x = this.pFinal.x();
+        double y = this.pFinal.y();
+        int v = (int)y;
+        while(x<=this.pInicial.x()){
+            if(v==y)
+                ps.add(new Punto(x,y));
+            x++;
+            y = y + pendiente();
+            v = (int)y;  
+        }      
+    }
+    return ps;
+    }
+}
+    
+
---
+++ talleres/taller01/README!!!.txt
@@ -0,0 +1,15 @@
+README!!!!!
+
+El programa es un menu que le permite interactuar con distintos
+tipos de datos abstractos, como puntos, fechas y lineas.
+
+Para inicializar el programa ejecute la clase "Ejecutar", al hacerlo
+se mostrara un pequeño menu, en el cual podra seleccionar el tipo de 
+dato abstracto con el que desea interactuar.
+
+Dependiendo de la opcion que elija se le pedira la informacion correspondiente,
+y se desplegara otro menu con las opciones que puede realizar.
+
+El resto de la funcionalidad es bastante intuitiva.
+
+
