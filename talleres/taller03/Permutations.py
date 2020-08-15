0e52bbc5085163c0af30aa29159aeccf0cb77075
avilladat <68928365+avilladat@users.noreply.github.com>
2020-08-15 00:38:28 -0500

    Update Permutations.py

--- talleres/taller03/Permutations.py
+++ talleres/taller03/Permutations.py
@@ -1,14 +1,3 @@
-#def subconjuntos(s):
- #   __subconjuntosbase__("",s)
-
-#def __subconjuntosbase__(base, t):
- #   if len(t)==0:
-  #      print(base)
-   # else:
-    #    __subconjuntosbase__(base+t[0], t[1:len(t)])
-     #   __subconjuntosbase__(base+t[1], t[0]+t[len(t)-1])
-      #  __subconjuntosbase__(base+t[2], t[0:2])
-
 #El siguiente hace llamado del metodo __subconjuntosbase__(base, t)
 def subconjuntos(s):
        __subconjuntosbase__("",s)
@@ -21,4 +10,4 @@ def __subconjuntosbase__(base, t):
         for i in range(len(t)):
             __subconjuntosbase__(base+t[i], t[0:i]+t[i+1:])    
 
-subconjuntos('')
+subconjuntos('abc')
